7d6c35eabcddccf464259828b2d745c81ff9c36e
pfig420 <palexandre09@ua.pt>
2021-12-22 17:33:23 +0000

    cleaning

--- WebServer/src/main/java/LifeManager/WebServer/DoctorRepository.java
+++
@@ -1,9 +0,0 @@
-package LifeManager.WebServer;
-
-import java.util.List;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface DoctorRepository extends JpaRepository<Doctor, Long>{
-    List<Doctor> findByMedicalID(Long medicalID);
-    List<Doctor> findByNameContainingIgnoreCase(String name);
-}
