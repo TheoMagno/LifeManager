65406ab58026e8670dfcf3f0686c4ef6f80e279e
pfig420 <palexandre09@ua.pt>
2021-12-22 14:42:23 +0000

    Beggining of docker compose

--- WebServer/src/main/java/LifeManager/WebServer/controller/Controller.java
+++ WebServer/src/main/java/LifeManager/WebServer/controller/Controller.java
@@ -11,6 +11,9 @@ import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
+import LifeManager.WebServer.service.*;
+import LifeManager.WebServer.model.*;
+
 @RestController
 public class Controller {
     //Doctors
