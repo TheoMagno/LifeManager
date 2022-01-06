907b5136ee13f4a835eaaf0966171f9389d2c65e
Marta <martasferreira@ua.pt>
2022-01-06 13:32:59 +0000

    broker upgrade

--- WebServer/src/main/java/LifeManager/WebServer/MessageBroker.java
+++ WebServer/src/main/java/LifeManager/WebServer/MessageBroker.java
@@ -9,6 +9,7 @@ import org.springframework.amqp.rabbit.annotation.RabbitListener;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 
+import java.util.HashMap;
 import java.util.LinkedList;
 import java.util.Optional;
 import java.util.Queue;
@@ -16,85 +17,119 @@ import java.util.Queue;
 @Component
 public class MessageBroker {
 
+    private HashMap<Integer, Queue> temperature = new HashMap<Integer, Queue>();
+    private HashMap<Integer, Queue> sugar = new HashMap<Integer, Queue>();
+    private HashMap<Integer, Queue> oxygen = new HashMap<Integer, Queue>();
+    private HashMap<Integer, Queue> heartbeat = new HashMap<Integer, Queue>();
+    private HashMap<Integer, Queue> blood_pressure = new HashMap<Integer, Queue>();
+
     @Autowired
     public SensorRepository sensorRepository;
 
     @RabbitListener(queues = "heartbeat")
     public void receive_heart_beat(String in) throws InterruptedException, ResourceNotFoundException {
-        Queue heart_beat_queue = new LinkedList<String>();
         JSONObject jo = new JSONObject(in);
-        double heart_beat = jo.getDouble("heartbeat");
-        if (heart_beat_queue.size() > 30) {
-            heart_beat_queue.poll();
+        double heart_beat = jo.getDouble("hearbeat");
+        int s_id = jo.getInt("id");
+        if(heartbeat.containsKey(s_id)){
+            Queue q = heartbeat.get(s_id);
+            if (q.size() > 30) {
+                q.poll();
+            }
+            q.add(heart_beat);
+            sugar.replace(s_id,q);
         }
-        heart_beat_queue.add(heart_beat);
-        System.out.print(heart_beat);
-        System.out.println(" was added to  heartbeat queue");
+        else{
+            Queue queue = new LinkedList<String>();
+            queue.add(heart_beat);
+            sugar.put(s_id,queue);
+        }
     }
 
     @RabbitListener(queues = "sugar_level")
     public void receive_sugar_level(String in) throws InterruptedException, ResourceNotFoundException {
-        Queue sugar_level_queue = new LinkedList<String>();
         JSONObject jo = new JSONObject(in);
         double sugar_level = jo.getDouble("sugar");
-        Integer id = jo.getInt("id");
-        if (sugar_level_queue.size() > 30) {
-            sugar_level_queue.poll();
+        int s_id = jo.getInt("id");
+        if(sugar.containsKey(s_id)){
+            Queue q = sugar.get(s_id);
+            if (q.size() > 30) {
+                q.poll();
+            }
+            q.add(sugar_level);
+            sugar.replace(s_id,q);
         }
-        sugar_level_queue.add(sugar_level);
-        System.out.print(sugar_level);
-        System.out.println(" was added to  sugar level queue");
-        Optional<Sensor> op = sensorRepository.findById(Long.valueOf(id));
-        if (op.isPresent()) {
-            Pacient p = op.get().getPatient();
+        else{
+            Queue queue = new LinkedList<String>();
+            queue.add(sugar_level);
+            sugar.put(s_id,queue);
         }
-
     }
 
     @RabbitListener(queues = "blood_pressure")
     public void receive_blood_pressure(String in) throws InterruptedException, ResourceNotFoundException {
-        Queue systolic_queue = new LinkedList<String>();
-        Queue diastolic_queue = new LinkedList<String>();
         JSONObject jo = new JSONObject(in);
         double systolic = jo.getDouble("systolic");
-        if (systolic_queue.size() > 30) {
-            systolic_queue.poll();
-        }
-        systolic_queue.add(systolic);
-        System.out.print(systolic);
-        System.out.println(" was added to  systolic queue");
         double diastolic = jo.getDouble("diastolic");
-        if (diastolic_queue.size() > 30) {
-            diastolic_queue.poll();
+        int s_id = jo.getInt("id");
+        if(blood_pressure.containsKey(s_id)){
+            Queue q = blood_pressure.get(s_id);
+            if (q.size() > 30) {
+                q.poll();
+            }
+            double[] bp = {systolic,diastolic};
+            q.add(bp);
+            blood_pressure.replace(s_id,q);
         }
-        diastolic_queue.add(diastolic);
-        System.out.print(diastolic);
-        System.out.println(" was added to  diastolic queue");
+        else{
+            Queue queue = new LinkedList<String[]>();
+            double[] bp = {systolic,diastolic};
+            queue.add(bp);
+            temperature.put(s_id,queue);
+        }
     }
-
 
     @RabbitListener(queues = "temperature")
     public void receive_body_temp(String in) throws InterruptedException, ResourceNotFoundException {
-        Queue body_temp_queue = new LinkedList<String>();
         JSONObject jo = new JSONObject(in);
         double body_temp = jo.getDouble("temperature");
-        if (body_temp_queue.size() > 30) {
-            body_temp_queue.poll();
+        int s_id = jo.getInt("id");
+        if(temperature.containsKey(s_id)){
+            Queue q = temperature.get(s_id);
+            if (q.size() > 30) {
+                q.poll();
+            }
+            q.add(body_temp);
+            temperature.replace(s_id,q);
         }
-        body_temp_queue.add(body_temp);
-        System.out.print(body_temp);
-        System.out.println(" was added to  body temperature queue");
+        else{
+            Queue queue = new LinkedList<String>();
+            queue.add(body_temp);
+            temperature.put(s_id,queue);
+        }
     }
 
     @RabbitListener(queues = "oxygen_level")
     public void receive_oxygen_level(String in) throws InterruptedException, ResourceNotFoundException {
-        Queue oxygen_level_queue = new LinkedList<String>();
-        JSONObject jo = new JSONObject(in);
-        double oxygen_level = jo.getDouble("temperature");
-        if (oxygen_level_queue.size() > 30) {
-            oxygen_level_queue.poll();
-        }
-        oxygen_level_queue.add(oxygen_level);
     }
 
+    public  Queue getBlood_pressure(Integer id) {
+        return blood_pressure.get(id);
+    }
+
+    public Queue getHeartbeat(Integer id) {
+        return heartbeat.get(id);
+    }
+
+    public Queue getTemperature(Integer id) {
+        return temperature.get(id);
+    }
+
+    public Queue getOxygen(Integer id) {
+        return oxygen.get(id);
+    }
+
+    public Queue getSugar(Integer id) {
+        return sugar.get(id);
+    }
 }
